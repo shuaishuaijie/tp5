0cc1fbe68dcbaca1fa4083911c0e3d81b6898e8c
shuaishuaijie <2418989583@qq.com>
2018-11-04 21:32:56 +0800

    css is too difficult

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 2018/11/4
+ * Time: 20:58
+ */
+
+namespace app\admin\controller;
+
+
+class Login{
+    public function index(){
+        //加载页面
+        return view();
+    }
+}
