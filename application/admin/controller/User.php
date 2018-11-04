86173c903eb70e030bd1e936bac2239fd9bc0e35
shuaishuaijie <2418989583@qq.com>
2018-11-04 10:57:23 +0800

    user

---
+++ application/admin/controller/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc
+ * Date: 2018/11/4
+ * Time: 10:38
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class User extends Controller{
+    //添加管理员
+    public function add(){
+        //加载页面
+        return view();
+    }
+}
