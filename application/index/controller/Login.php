80adce4bbbb447350680bbbb659cfb1f1bcdf4ed
shuaishuaijie <2418989583@qq.com>
2018-11-24 16:21:57 +0800

    login+ajax

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -18,10 +18,31 @@ class Login extends Controller
      */
     public function index()
     {
-       return $this->fetch();
+        if (request()->isPost()) {
+
+            $data = input('post');
+            $result = json_decode($data);
+            unset($result['code']);
+            $res = db('user')->where('login', $result['login'])->find();
+
+            $responseText = ["Status" => "ok", "Text" => "登录成功"];
+            //$responseText = ["Status"=> "error", "Text"=>"账号或密码错误请重新登录"];
+            return json($responseText);
+        }
+        return $this->fetch();
     }
+
     //
-    public function test(){
-        return json("ajax成功！");
+    public function test()
+    {
+        $data = [
+            "account"=> 2016301020213,
+            "password"=> 123456,
+            "name"=> "杰哥",
+            "clas" => 1,
+            "score"=> null,
+            "photo" => null,
+        ];
+        db('user')->insert($data);
     }
 }
